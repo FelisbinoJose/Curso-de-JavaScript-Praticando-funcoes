3f1c7d1d8694711222cea580f476aa7c6103f181
FelisbinoJose <246760.jose@unibave.net>
2026-02-03 17:28:08 -0300

    FIX: ajuste na importação da função e nas condições

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const verificarParidade = (numero) => {
 
-    if(typeof numero !== "number" || numero === 2) {
+    if(typeof numero !== "number" || numero === 0) {
         throw new Error("Os valores devem ser número");
     }
 
     return numero % 2 == 0 ? "Par" : "Ímpar";
 }
 
-modulo.export = verificarParidade;
+module.exports = verificarParidade;
 
