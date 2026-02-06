32ebf22710f14915735a93f491e2da1dbcc3f1a4
FelisbinoJose <246760.jose@unibave.net>
2026-02-06 19:19:58 -0300

    FEAT: adiciona solução do exercício

---
+++ index.js
@@ -0,0 +1,17 @@
+const processarPedido =  (nomeCliente, tipoCliente, mensagemPersonalizada,) => {
+    const mensagemPadrao = `Processando pedido ${nomeCliente}...\n`;
+    return mensagemPadrao + mensagemPersonalizada(nomeCliente, tipoCliente)
+}
+
+function mensagemPersonalizada(nomeCliente, tipoCliente) {
+
+    if (tipoCliente === "vip") {
+        return `Obrigado pela preferência, ${nomeCliente}! Você ganhou frete grátis`;
+
+    } else if (tipoCliente === "novo") {
+        return `Bem-vindo(a), ${nomeCliente} Aproveite um cupom de boas-vindas.`
+    } else {
+        return `Òbrigad pela sua compra, ${nomeCliente}`
+    }
+}
+
